1432e7ae837dea128e8adadf6f64f9db297ab74a
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2020-02-18 14:20:43 +0000

    Add temperature IGP experiment

--- run_experiments.sh
+++ run_experiments.sh
@@ -6,5 +6,6 @@ python experiments/eeg_ilmm.py
 python experiments/exchange.py
 python experiments/exchange_ilmm.py
 python experiments/temperature.py
+python experiments/temperature_igp.py
 python experiments/simulators.py
 python experiments/simulators_process.py
